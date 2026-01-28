1cc63f2b3278b9f46c9badece9afdb643891241f
molinax18 <arielmolina8953@gmail.com>
2026-01-28 14:50:36 -0300

    feat(style): new @layer components for ThemeContext

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
+import { useThemeContext } from "./contexts/ThemeContext";
+
 function App() {
-  return <div>App</div>;
+  const { theme } = useThemeContext();
+  const background = theme === "dark" ? "bg-dark" : "bg-semi-light";
+
+  return (
+    <div className={`min-w-dvh min-h-dvh grid grid-rows-3 ${background}`}>
+      App
+    </div>
+  );
 }
 
 export default App;
