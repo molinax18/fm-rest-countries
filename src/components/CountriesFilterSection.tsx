f03043b661de7b7ea67b51034c710ee92925d407
molinax18 <arielmolina8953@gmail.com>
2026-01-29 17:42:34 -0300

    chore(CountriesFilterSection): remove tag

--- src/components/CountriesFilterSection.tsx
+++ src/components/CountriesFilterSection.tsx
@@ -1,4 +1,4 @@
-import { useState, type ChangeEvent, type SubmitEvent } from "react";
+import { useState, type ChangeEvent } from "react";
 import { BsArrowDownShort } from "react-icons/bs";
 import SearchInput from "./SearchInput";
 import SelectFilter from "./SelectFilter";
@@ -10,32 +10,24 @@ export default function CountriesFilterSection() {
   function handleSearch(event: ChangeEvent<HTMLInputElement>) {
     return setSearch(event.target.value);
   }
-  function onSubmit(event: SubmitEvent<HTMLFormElement>) {
-    event.preventDefault();
-  }
 
   return (
-    <section className="container">
-      <form
-        className="flex flex-col gap-y-6 sm:flex-row sm:justify-between"
-        onSubmit={onSubmit}
-      >
-        <SearchInput
-          onChange={handleSearch}
-          value={search}
-          placeholder="Search for a country..."
-        />
+    <section className="flex flex-col gap-y-6 sm:flex-row sm:justify-between container">
+      <SearchInput
+        onChange={handleSearch}
+        value={search}
+        placeholder="Search for a country..."
+      />
 
-        <SelectFilter
-          className="w-max"
-          options={regions}
-          label={
-            <>
-              Filter by Region <BsArrowDownShort />
-            </>
-          }
-        />
-      </form>
+      <SelectFilter
+        className="w-max"
+        options={regions}
+        label={
+          <>
+            Filter by Region <BsArrowDownShort />
+          </>
+        }
+      />
     </section>
   );
 }
